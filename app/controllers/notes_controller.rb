89b8be7879634a03165959eca0332bc4dcb84113
Miika Leppänen <maleppanen@gmail.com>
2012-11-07 17:30:46 +0200

    Add org link to item if the first line of the body is url.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  include ApplicationHelper
   before_filter :authenticate_user!
   skip_before_filter :verify_authenticity_token
   before_filter :skip_trackable
@@ -13,8 +14,24 @@ class NotesController < ApplicationController
       parent_rev = metadata["rev"]
     rescue DropboxError => de
     end
-    contents.insert(0, "#{params["body-plain"]}\n\n")
-    contents.insert(0, "* #{params["subject"]}\n")
+
+    new_content = ""
+    body = params["body-plain"].strip
+    body.gsub!(/\r\n/, "\n")
+    enum = body.lines
+    begin
+      first_line = enum.next
+      if uri?(first_line)
+        new_content << "* [[#{first_line.strip}][#{params["subject"]}]]\n"
+      else
+        new_content << "* #{params["subject"]}\n"
+        new_content << first_line
+      end
+      while new_content << enum.next; end
+    rescue StopIteration => si
+    end
+
+    contents.insert(0, "#{new_content}\n\n")
 
     client.put_file('/notes.org', contents, true, parent_rev)
 
